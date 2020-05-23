1f9f57e10ae41013cb942e7b1be2c29a7669126e
simeji <summercontinues0000@gmail.com>
2020-05-23 08:10:39 +0000

    Add detailed comment

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ApplicationRecord
+  
+  # Userモデルに関連するfollowモデルを疑似的に定義した上でリレーションする
   belongs_to :user
   belongs_to :follow, class_name: 'User'
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
   
+  # ====================自分がフォローしているユーザーとの関連 ===================================
+  # 自分がフォローした相手(follow_id)を集めるテーブルなので、親はuser_idになる
+  # primary_keyと一致するのでそのままでOK(foreign_key: 'user_id'は省かれている)
   has_many :relationships
+  
+  # relationshipsテーブルを介してfollowモデルからfollow_idを集めることをfollowingと定義する
   has_many :followings, through: :relationships, source: :follow
+  # ==============================================================================================
+  
+  # ====================自分がフォローされるユーザーとの関連 =====================================
+  # Relationshipモデルに関連するreverse_of_relationshipモデルを疑似的に定義した上でリレーションする
+  # 自分をフォローしているフォロワー(user_id)を集めるテーブルなので、親はfollow_idになる
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  
+  # reverse_of_relationshipsテーブルを介してuserモデルからuser_idを集めることをfollowersと定義する
   has_many :followers, through: :reverse_of_relationships, source: :user
+  # ==============================================================================================
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, {presence: true}
